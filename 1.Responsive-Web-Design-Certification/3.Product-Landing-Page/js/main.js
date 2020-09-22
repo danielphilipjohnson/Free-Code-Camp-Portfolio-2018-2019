d67035d391aa6c5b8529b7b5ed21ad7c4e49df5e
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-09-22 22:51:26 +0100

    Finished website

--- 1.Responsive-Web-Design-Certification/3.Product-Landing-Page/js/main.js
+++ 1.Responsive-Web-Design-Certification/3.Product-Landing-Page/js/main.js
@@ -22,13 +22,11 @@ window.addEventListener("resize", displayWindowSize);
 window.onscroll = function () {
   if (window.pageYOffset > 80) {
     navbar.classList.remove("top");
-    if (!scrolled) {
-      // navbar.style.transform = "translateY(0)";
-    }
+
     setTimeout(function () {
-      // navbar.style.transform = "translateY(0px)";
       scrolled = true;
     }, 100);
+    
   } else {
     navbar.classList.add("top");
     scrolled = false;
