6049d98a75c4aac4e14be35f3d4b44e93488ca56
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-09-22 16:32:00 +0100

    Moved js to a seperate file

---
+++ 1.Responsive-Web-Design-Certification/3.Product-Landing-Page/js/main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var x = document.getElementById("nav-sub");
+  console.log(x);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
