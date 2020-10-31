2c2797c36782270377d5f91e9a80c55a843c8e52
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-10-31 16:30:50 +0000

    Display of soundbank change

--- 2.Front-End-Libraries-Certification/3. Build a Drum Machine/src/Components/Display.js
+++ 2.Front-End-Libraries-Certification/3. Build a Drum Machine/src/Components/Display.js
@@ -1,36 +1,26 @@
-import React, {Component} from 'react';
-import Controls from './Controls/Controls';
+import React, { Component } from "react";
+import Controls from "./Controls/Controls";
+import "./Display.css";
+
+import logo from "../img/waves.webp";
 
 class Display extends Component {
+  render() {
+    return (
+      <header id="display" className="top">
+        <div className="drum-display">
+          <h2 className="drum-display__title">{this.props.text}</h2>
+          <img src={logo} alt="Logo" />
+   
 
-    render() {
-        return (
-            <header id="display" className="top">
-               
-                <div className="Drum-display">
-                <button className="power-button">Off</button>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Previous</th>
-                                <th>Next</th>
-                                <th>*</th>
-                                <th>Load</th>
-                            </tr>
-                        </thead>
-                    </table>
-                    <div className="categories">
-                        <h2 className="logo">{this.props.text}</h2>
-
-                        <Controls
-                            updateSoundBankAndDisplay={this.props.updateSoundBankAndDisplay}
-                            bankSets={this.props.bankSets}/>
-
-                    </div>
-                </div>
-            </header>
-        )
-    }
+          <Controls
+            updateSoundBankAndDisplay={this.props.updateSoundBankAndDisplay}
+            bankSets={this.props.bankSets}
+          />
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Display;
